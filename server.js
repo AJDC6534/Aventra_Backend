8d37a4587e37e583207c52ae1e40ea33844fbb0c
AJDC6534 <sinon6534@gmail.com>
2025-07-09 02:11:06 +0400

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    next();
+});
 app.use(cors({
   origin: [
     'http://localhost:3000',
