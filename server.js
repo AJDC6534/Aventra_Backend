ba4a8f8332e05a2b50526d6dcf5a58970306b7ab
AJDC6534 <sinon6534@gmail.com>
2025-07-09 01:45:46 +0400

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://github.com/AJDC6534/Aventra.git'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
